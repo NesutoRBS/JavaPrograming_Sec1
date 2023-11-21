13bd0ba7ee6e6f63fbb62924c3bf96e7de804c4c
NesutoRBS <nesuto.rbs@gmail.com>
2023-11-21 11:54:34 +0700

    LabSheet2

---
+++ JavaProject_Lab2/src/Lab101.java
@@ -0,0 +1,27 @@
+import java.text.*;
+import java.util.*;
+
+public class Lab101 {
+
+	public static void main(String[] args) {
+		DecimalFormat frm = new DecimalFormat("#,###.00");
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Input Product Name   : ");
+		String productName = scan.nextLine();
+		System.out.print("Input Product unit   : ");
+		int productUnit = scan.nextInt();
+		System.out.print("Input Price per unit : ");
+		double pricePerUnit = scan.nextDouble();
+		System.out.println("---------------------------------------");
+		double totalPrice = productUnit * pricePerUnit;
+		System.out.println("Total Price is " + frm.format(totalPrice) + " baht.");
+		System.out.println("---------------------------------------");
+		System.out.print("How many discount (%) : ");
+		int discount = scan.nextInt();
+		System.out.println("---------------------------------------");
+		System.out.println("Discount from "+ discount + "(%)\t" + frm.format(totalPrice*discount/100) + " baht.");
+		System.out.println("Amount to be paid \t" + frm.format(totalPrice*(100-discount)/100) + " baht.");
+
+	}
+NO
+}
