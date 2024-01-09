dad8d7db1f293a0118a8a7c815a3c7ecb44022cb
NesutoRBS <nesuto.rbs@gmail.com>
2024-01-09 14:07:46 +0700

    JavaLab7

--- JavaProject_Lab7/src/Lab706.java
+++ JavaProject_Lab7/src/Lab706.java
@@ -1,4 +1,4 @@
-/*import javax.swing.*;
+import javax.swing.*;
 
 public class Lab706 {
 	
@@ -6,33 +6,42 @@ public class Lab706 {
 		int[] nums = {25,78,41,22,36,85,37};
 		int inputNum = Integer.parseInt(
 				JOptionPane.showInputDialog("Input index of array:"));
-		while(inputNum < 0 || inputNum > 6) {
+		while(!checkIndex(nums,inputNum)) {
 			inputNum = Integer.parseInt(
 					JOptionPane.showInputDialog("Input index of array, again:"));
 		}
+		
+		int currentValue = currentData(nums,inputNum);
+		int prevValue = prevData(nums,inputNum);
+		int nextValue = nextData(nums,inputNum);
+		
 		JOptionPane.showMessageDialog(null,
-				"Current data, nums[" + currentData(nums,inputNum) + " is " + nums[currentData(nums,inputNum)] +
-				"Previous data, nums[" + prevData(nums,inputNum) + " is " + nums[prevData(nums,inputNum)] +
-				"Next data, nums[" + nextData(nums,inputNum) + " is " + nums[nextData(nums,inputNum)]
+				"Current data, nums[" + inputNum + "] is " + currentValue +
+				( (prevData(nums,inputNum) >= 0) ?
+						("\nPrevious data, nums[" + prevValue + "] is " + nums[prevValue])
+						:("\nNo previous Data")) +
+				( (nextData(nums,inputNum) < 7)?
+						("\nNext data, nums[" + nextValue + "] is " + nums[nextValue])
+						: ("\nNo next Data"))
 				);
 
 	}
 	
 	public static boolean checkIndex(int[] nums, int index) {
-		
+		return (index >= 0 && index < nums.length) ? true : false; 
 	}
 	
 	public static int currentData(int[] nums, int index) {
-		
+		return nums[index];
 	}
 	
 	public static int prevData(int[] nums, int index) {
-		
+		return index-1;
 	}
 	
 	public static int nextData(int[] nums, int index) {
-		
+		return index+1;
 	}
 
 }
-*/
+
